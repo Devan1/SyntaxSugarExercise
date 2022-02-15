d073d93b6600c0877800b6097b3ac508bc014be0
Devan <deshlock010101@hotmai.com>
2022-02-15 12:03:56 -0600

    completed the exercise

--- SyntaxSugarExercise/Program.cs
+++ SyntaxSugarExercise/Program.cs
@@ -6,7 +6,50 @@ namespace SyntaxSugarExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+            //Explicit Typing
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+                Console.Write(response);
+            }
+            else
+            {
+                response = answer + " greater than or equal to nine";
+                Console.Write(response);
+            }
+            Console.WriteLine();
+            
+            
+            
+            //Inferred Typing
+            var answerInferred = 4;
+            var responseInferred = "";
+
+            if(answer < 9)
+            {
+                responseInferred = answerInferred + " is less than nine";
+                Console.Write(responseInferred);
+            }
+            else
+            {
+                responseInferred = answerInferred + " greater than or equal to nine";
+                Console.Write(responseInferred);
+            }
+            Console.WriteLine();
+
+            
+            
+            //Inline If/Ternary Operator Typing
+            var answerInline = 4;
+            var responseInline = (answerInline < 9) ? answerInline + " is less than nine" : answerInline + " greater than or equal to nine";
+            Console.Write(responseInline);
+
+
         }
     }
 }
